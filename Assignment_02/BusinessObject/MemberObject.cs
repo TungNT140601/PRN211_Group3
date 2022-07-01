84add04569693045388405b2b898d428e8418488
DaoAnhTu21 <105470764+DaoAnhTu21@users.noreply.github.com>
2022-07-01 19:58:49 +0700

    daoanhtu

--- Assignment_02/BusinessObject/MemberObject.cs
+++ Assignment_02/BusinessObject/MemberObject.cs
@@ -2,6 +2,11 @@
 {
     public class MemberObject
     {
-
+        public int MemberId { get; set; }
+        public string Email { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Password { get; set; }
     }
 }
