f072ac0dc3aacb9dafb0753802f0fca6e9009bab
DaoAnhTu21 <105470764+DaoAnhTu21@users.noreply.github.com>
2022-07-02 21:47:02 +0700

    reupPart3

--- Assignment_02/DataAccess/MemberDAO.cs
+++ Assignment_02/DataAccess/MemberDAO.cs
@@ -3,12 +3,18 @@ using System.Data;
 using Microsoft.Extensions.Configuration;
 using BusinessObject;
 using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
 namespace DataAccess
 {
-    public class MemberDAO
+    public class MemberDAO : BaseDAL
     {
         private static MemberDAO instance = null;
         private static readonly object instanceLock = new object();
+        private MemberDAO()
+        {
+
+        }
         public static MemberDAO Instance
         {
             get
@@ -23,129 +29,6 @@ namespace DataAccess
                 }
             }
         }
-        public MemberDAO()
-        {
-            var connectionString = GetConnectionString();
-            m = new MemberDAO(connectionString);
-        }
-        public MemberDAO m { get; set; } = null;
-        public SqlConnection connection = null;
-        public string GetConnectionString()
-        {
-            string connectionString;
-            IConfiguration config = new ConfigurationBuilder()
-                                        .SetBasePath(Directory.GetCurrentDirectory())
-                                        .AddJsonFile("appsetting.json", true, true)
-                                        .Build();
-            connectionString = config["ConnectionString:FStoreDB"];
-            return connectionString;
-        }
-        public void CloseConnection() => m.CloseConnection(connection);
-
-        private string ConnectionString { get; set; }
-        public MemberDAO(string connectionString) => ConnectionString = connectionString;
-        public void CloseConnection(SqlConnection connection) => connection.Close();
-        public SqlParameter CreateParameter(string name, int size, object value, DbType dbType, ParameterDirection direction = ParameterDirection.Input)
-        {
-            return new SqlParameter
-            {
-                DbType = dbType,
-                ParameterName = name,
-                Size = size,
-                Direction = direction,
-                Value = value,
-            };
-        }
-        public IDataReader GetDataReader(string commandText, CommandType commandType, out SqlConnection connection, params SqlParameter[] parameters)
-        {
-            IDataReader reader = null;
-            try
-            {
-                connection = new SqlConnection(ConnectionString);
-                connection.Open();
-                var command = new SqlCommand(commandText, connection);
-                command.CommandType = commandType;
-                if (parameters != null)
-                {
-                    foreach (var parameter in parameters)
-                    {
-                        command.Parameters.Add(parameter);
-                    }
-                }
-                reader = command.ExecuteReader();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            return reader;
-        }
-        public void Delete(string commandText, CommandType commandType, params SqlParameter[] parameters)
-        {
-            try
-            {
-                using var connection = new SqlConnection(ConnectionString);
-                connection.Open();
-                using var command = new SqlCommand(commandText, connection);
-                command.CommandType = commandType;
-                if (parameters != null)
-                {
-                    foreach (var parameter in parameters)
-                    {
-                        command.Parameters.Add(parameter);
-                    }
-                }
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        public void Insert(string commandText, CommandType commandType, params SqlParameter[] parameters)
-        {
-            try
-            {
-                using var connection = new SqlConnection(ConnectionString);
-                connection.Open();
-                using var command = new SqlCommand(commandText, connection);
-                command.CommandType = commandType;
-                if (parameters != null)
-                {
-                    foreach (var parameter in parameters)
-                    {
-                        command.Parameters.Add(parameter);
-                    }
-                }
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        public void Update(string commandText, CommandType commandType, params SqlParameter[] parameters)
-        {
-            try
-            {
-                using var connection = new SqlConnection(ConnectionString);
-                connection.Open();
-                using var command = new SqlCommand(commandText, connection);
-                command.CommandType = commandType;
-                if (parameters != null)
-                {
-                    foreach (var parameter in parameters)
-                    {
-                        command.Parameters.Add(parameter);
-                    }
-                }
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
 
         public IEnumerable<MemberObject> GetMemberList()
         {
@@ -154,7 +37,7 @@ namespace DataAccess
             var mem = new List<MemberObject>();
             try
             {
-                dataReader = GetDataReader(SQLSelect, CommandType.Text, out connection);
+                dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection);
                 while (dataReader.Read())
                 {
                     mem.Add(new MemberObject
@@ -187,8 +70,8 @@ namespace DataAccess
             string SQLSelect = "SELECT MemberId, Email, Companyname, City, Country, Password FROM Member WHERE MemberId = @MemberId";
             try
             {
-                var param = CreateParameter("@MemberId", 4, memID, DbType.Int32);
-                dataReader = GetDataReader(SQLSelect, CommandType.Text, out connection, param);
+                var param = dataProvider.CreateParameter("@MemberId", 4, memID, DbType.Int32);
+                dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection, param);
                 if (dataReader.Read())
                 {
                     mem = new MemberObject
@@ -221,8 +104,8 @@ namespace DataAccess
             try
             {
                 string SQLSelect = "SELECT MemberId, Email, Companyname, City, Country, Password FROM Member WHERE Email = @Email";
-                var mail = CreateParameter("@Email", 50, email, DbType.String);
-                dataReader = GetDataReader(SQLSelect, CommandType.Text, out connection, mail);
+                var mail = dataProvider.CreateParameter("@Email", 50, email, DbType.String);
+                dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection, mail);
                 if (dataReader.Read())
                 {
                     check = true;
@@ -248,10 +131,10 @@ namespace DataAccess
                 if(Check(email) == true)
                 {
                     string SQLSelect = "SELECT MemberId, Email, Companyname, City, Country, Password FROM Member WHERE Email = @Email AND Password = @Password";
-                    var mail = CreateParameter("@Email", 50, email, DbType.String);
-                    var pass = CreateParameter("@Password", 50, password, DbType.String);
-                    dataReader = GetDataReader(SQLSelect, CommandType.Text, out connection, mail); ;
-                    dataReader = GetDataReader(SQLSelect, CommandType.Text, out connection, pass);
+                    var mail = dataProvider.CreateParameter("@Email", 50, email, DbType.String);
+                    var pass = dataProvider.CreateParameter("@Password", 50, password, DbType.String);
+                    dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection, mail); ;
+                    dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection, pass);
                     if (dataReader.Read())
                     {
                         mem = new MemberObject
@@ -286,13 +169,13 @@ namespace DataAccess
                 {
                     string SQLInsert = "INSERT Member values(@MemberId, @Email, @Companyname, @City, @Country, @Password)";
                     var parameters = new List<SqlParameter>();
-                    parameters.Add(CreateParameter("@MemberId", 4, mem.MemberId, DbType.Int32));
-                    parameters.Add(CreateParameter("@Email", 4, mem.Email, DbType.String));
-                    parameters.Add(CreateParameter("@Companyname", 50, mem.CompanyName, DbType.String));
-                    parameters.Add(CreateParameter("@City", 50, mem.City, DbType.String));
-                    parameters.Add(CreateParameter("@Country", 50, mem.Country, DbType.String));
-                    parameters.Add(CreateParameter("@Password", 4, mem.Password, DbType.String));
-                    Insert(SQLInsert, CommandType.Text, parameters.ToArray());
+                    parameters.Add(dataProvider.CreateParameter("@MemberId", 4, mem.MemberId, DbType.Int32));
+                    parameters.Add(dataProvider.CreateParameter("@Email", 4, mem.Email, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Companyname", 50, mem.CompanyName, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@City", 50, mem.City, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Country", 50, mem.Country, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Password", 4, mem.Password, DbType.String));
+                    dataProvider.Insert(SQLInsert, CommandType.Text, parameters.ToArray());
                 }
                 else
                 {
@@ -318,13 +201,13 @@ namespace DataAccess
                 {
                     string SQLUpdate = "UPDATE Member set Email = @Email,Companyname = @Companyname,City = @City, Country = @Country, Password = @Password WHERE MemberId = @MemberId";
                     var parameters = new List<SqlParameter>();
-                    parameters.Add(CreateParameter("@MemberId", 4, mem.MemberId, DbType.Int32));
-                    parameters.Add(CreateParameter("@Email", 4, mem.Email, DbType.String));
-                    parameters.Add(CreateParameter("@Companyname", 50, mem.CompanyName, DbType.String));
-                    parameters.Add(CreateParameter("@City", 50, mem.City, DbType.String));
-                    parameters.Add(CreateParameter("@Country", 50, mem.Country, DbType.String));
-                    parameters.Add(CreateParameter("@Password", 4, mem.Password, DbType.String));
-                    Insert(SQLUpdate, CommandType.Text, parameters.ToArray());
+                    parameters.Add(dataProvider.CreateParameter("@MemberId", 4, mem.MemberId, DbType.Int32));
+                    parameters.Add(dataProvider.CreateParameter("@Email", 4, mem.Email, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Companyname", 50, mem.CompanyName, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@City", 50, mem.City, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Country", 50, mem.Country, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@Password", 4, mem.Password, DbType.String));
+                    dataProvider.Insert(SQLUpdate, CommandType.Text, parameters.ToArray());
                 }
                 else
                 {
@@ -348,8 +231,8 @@ namespace DataAccess
                 if (member != null)
                 {
                     string SQLDelete = "DELETE Member WHERE MemberId = @MemberId";
-                    var param = CreateParameter("@MemberId", 4, memID, DbType.Int32);
-                    Delete(SQLDelete, CommandType.Text, param);
+                    var param = dataProvider.CreateParameter("@MemberId", 4, memID, DbType.Int32);
+                    dataProvider.Delete(SQLDelete, CommandType.Text, param);
                 }
                 else
                 {
