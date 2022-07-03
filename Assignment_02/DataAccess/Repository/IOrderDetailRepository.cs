e1db53332d6f756ab08549d6a5f7cdec2b6cbb8c
vuncse151184 <vu12ace3@gmail.com>
2022-07-03 16:19:25 +0700

    update orderdetail

--- Assignment_02/DataAccess/Repository/IOrderDetailRepository.cs
+++ Assignment_02/DataAccess/Repository/IOrderDetailRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessObject;
 namespace DataAccess.Repository
 {
-    internal class IOrderDetailRepository
+    public interface IOrderDetailRepository
     {
+        IEnumerable<OrderDetailDAO> GetOrderDetailList ();
+        OrderDetailDAO GetOrderDetailByOrderID(int orderID);
+        void InsertOrderDetail(OrderDetailDAO order);
+        void UpdateOrderDetail(OrderDetailDAO order);
+        void DeleteOrderDetail(int orderID);
+ 
     }
+
+
 }
--- Assignment_02/DataAccess/Repository/OrderDetailRepository.cs
+++ Assignment_02/DataAccess/Repository/OrderDetailRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
-
+using BusinessObject;
 namespace DataAccess.Repository
 {
-    internal class OrderDetailRepository
+    public class OrderDetailRepository : IOrderDetailRepository
     {
+        public OrderDetailObject GetOrderDetailByOrderID(int orderID) => OrderDetailDAO.Instance.GetOrderDetailByOrderId(orderID);
+        public IEnumerable<OrderDetailObject> GetOrderDetail() => OrderDetailDAO.Instance.getOrderDetailList();
+        public void InsertOrderDetail(OrderDetailObject orderdetail) => OrderDetailDAO.Instance.AddNew(orderdetail);
+        public void DeleteOrderDetail(int orderId) => OrderDetailDAO.Instance.Remove(orderId);
+        public void UpdateOrderDetail(OrderDetailObject order) => OrderDetailDAO.Instance.Update(order);
+
+
     }
 }
