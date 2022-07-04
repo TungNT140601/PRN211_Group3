8354c9285eb169606396f79a5b9f66d548a88e75
TungNT140601 <tungnt14062001@gmail.com>
2022-07-04 08:46:29 +0700

    up file

--- Assignment2/SalesWinApp/frmMain.Designer.cs
+++ Assignment2/SalesWinApp/frmMain.Designer.cs
@@ -33,7 +33,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.productsMenu = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewAllProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnViewAllProduct = new System.Windows.Forms.ToolStripMenuItem();
             this.ProductManagement = new System.Windows.Forms.ToolStripMenuItem();
             this.btnViewProductAdmin = new System.Windows.Forms.ToolStripMenuItem();
             this.btnAddNewProductAdmin = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,7 +46,7 @@
             this.btnViewAllMemberAdmin = new System.Windows.Forms.ToolStripMenuItem();
             this.btnAddNewMemberAdmin = new System.Windows.Forms.ToolStripMenuItem();
             this.btnLogout = new System.Windows.Forms.ToolStripMenuItem();
-            this.lbHello = new System.Windows.Forms.Label();
+            this.lbHello = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,10 +58,11 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.lbHello});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1153, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1153, 29);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,25 +72,26 @@
             this.productsMenu,
             this.ordersMenu,
             this.memberMenu});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 25);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // productsMenu
             // 
             this.productsMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.viewAllProductToolStripMenuItem,
+            this.btnViewAllProduct,
             this.ProductManagement});
             this.productsMenu.Name = "productsMenu";
-            this.productsMenu.Size = new System.Drawing.Size(180, 22);
+            this.productsMenu.Size = new System.Drawing.Size(180, 26);
             this.productsMenu.Text = "Products";
             // 
-            // viewAllProductToolStripMenuItem
+            // btnViewAllProduct
             // 
-            this.viewAllProductToolStripMenuItem.Name = "viewAllProductToolStripMenuItem";
-            this.viewAllProductToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
-            this.viewAllProductToolStripMenuItem.Text = "View All Product";
-            this.viewAllProductToolStripMenuItem.Click += new System.EventHandler(this.viewAllProductToolStripMenuItem_Click);
+            this.btnViewAllProduct.Name = "btnViewAllProduct";
+            this.btnViewAllProduct.Size = new System.Drawing.Size(230, 26);
+            this.btnViewAllProduct.Text = "View All Product";
+            this.btnViewAllProduct.Click += new System.EventHandler(this.btnViewAllProduct_Click);
             // 
             // ProductManagement
             // 
@@ -97,20 +99,20 @@
             this.btnViewProductAdmin,
             this.btnAddNewProductAdmin});
             this.ProductManagement.Name = "ProductManagement";
-            this.ProductManagement.Size = new System.Drawing.Size(190, 22);
+            this.ProductManagement.Size = new System.Drawing.Size(230, 26);
             this.ProductManagement.Text = "Product Management";
             // 
             // btnViewProductAdmin
             // 
             this.btnViewProductAdmin.Name = "btnViewProductAdmin";
-            this.btnViewProductAdmin.Size = new System.Drawing.Size(180, 22);
+            this.btnViewProductAdmin.Size = new System.Drawing.Size(202, 26);
             this.btnViewProductAdmin.Text = "View All Products";
             this.btnViewProductAdmin.Click += new System.EventHandler(this.btnViewProductAdmin_Click);
             // 
             // btnAddNewProductAdmin
             // 
             this.btnAddNewProductAdmin.Name = "btnAddNewProductAdmin";
-            this.btnAddNewProductAdmin.Size = new System.Drawing.Size(180, 22);
+            this.btnAddNewProductAdmin.Size = new System.Drawing.Size(202, 26);
             this.btnAddNewProductAdmin.Text = "Add New Product";
             this.btnAddNewProductAdmin.Click += new System.EventHandler(this.btnAddNewProductAdmin_Click);
             // 
@@ -120,13 +122,13 @@
             this.viewHistoryOrder,
             this.orderManagement});
             this.ordersMenu.Name = "ordersMenu";
-            this.ordersMenu.Size = new System.Drawing.Size(180, 22);
+            this.ordersMenu.Size = new System.Drawing.Size(180, 26);
             this.ordersMenu.Text = "Orders";
             // 
             // viewHistoryOrder
             // 
             this.viewHistoryOrder.Name = "viewHistoryOrder";
-            this.viewHistoryOrder.Size = new System.Drawing.Size(180, 22);
+            this.viewHistoryOrder.Size = new System.Drawing.Size(217, 26);
             this.viewHistoryOrder.Text = "View History Order";
             this.viewHistoryOrder.Click += new System.EventHandler(this.viewHistoryOrder_Click);
             // 
@@ -135,13 +137,13 @@
             this.orderManagement.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnViewAllOrderAdmin});
             this.orderManagement.Name = "orderManagement";
-            this.orderManagement.Size = new System.Drawing.Size(180, 22);
+            this.orderManagement.Size = new System.Drawing.Size(217, 26);
             this.orderManagement.Text = "Order Management";
             // 
             // btnViewAllOrderAdmin
             // 
             this.btnViewAllOrderAdmin.Name = "btnViewAllOrderAdmin";
-            this.btnViewAllOrderAdmin.Size = new System.Drawing.Size(180, 22);
+            this.btnViewAllOrderAdmin.Size = new System.Drawing.Size(181, 26);
             this.btnViewAllOrderAdmin.Text = "View All Order";
             this.btnViewAllOrderAdmin.Click += new System.EventHandler(this.btnViewAllOrderAdmin_Click);
             // 
@@ -151,7 +153,7 @@
             this.memberManagementToolStripMenuItem,
             this.btnLogout});
             this.memberMenu.Name = "memberMenu";
-            this.memberMenu.Size = new System.Drawing.Size(180, 22);
+            this.memberMenu.Size = new System.Drawing.Size(180, 26);
             this.memberMenu.Text = "Member";
             // 
             // memberManagementToolStripMenuItem
@@ -160,46 +162,44 @@
             this.btnViewAllMemberAdmin,
             this.btnAddNewMemberAdmin});
             this.memberManagementToolStripMenuItem.Name = "memberManagementToolStripMenuItem";
-            this.memberManagementToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.memberManagementToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
             this.memberManagementToolStripMenuItem.Text = "Member Management";
             // 
             // btnViewAllMemberAdmin
             // 
             this.btnViewAllMemberAdmin.Name = "btnViewAllMemberAdmin";
-            this.btnViewAllMemberAdmin.Size = new System.Drawing.Size(180, 22);
+            this.btnViewAllMemberAdmin.Size = new System.Drawing.Size(207, 26);
             this.btnViewAllMemberAdmin.Text = "View All Members";
             this.btnViewAllMemberAdmin.Click += new System.EventHandler(this.btnViewAllMemberAdmin_Click);
             // 
             // btnAddNewMemberAdmin
             // 
             this.btnAddNewMemberAdmin.Name = "btnAddNewMemberAdmin";
-            this.btnAddNewMemberAdmin.Size = new System.Drawing.Size(180, 22);
+            this.btnAddNewMemberAdmin.Size = new System.Drawing.Size(207, 26);
             this.btnAddNewMemberAdmin.Text = "Add New Member";
             this.btnAddNewMemberAdmin.Click += new System.EventHandler(this.btnAddNewMemberAdmin_Click);
             // 
             // btnLogout
             // 
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(193, 22);
+            this.btnLogout.Size = new System.Drawing.Size(235, 26);
             this.btnLogout.Text = "Logout";
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // lbHello
             // 
-            this.lbHello.AutoSize = true;
-            this.lbHello.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbHello.Location = new System.Drawing.Point(12, 46);
+            this.lbHello.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lbHello.Name = "lbHello";
-            this.lbHello.Size = new System.Drawing.Size(0, 37);
-            this.lbHello.TabIndex = 2;
+            this.lbHello.Size = new System.Drawing.Size(58, 25);
+            this.lbHello.Text = "Hello";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1153, 730);
-            this.Controls.Add(this.lbHello);
             this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -221,7 +221,7 @@
         private ToolStripMenuItem memberMenu;
         private ToolStripMenuItem productsMenu;
         private ToolStripMenuItem ordersMenu;
-        private ToolStripMenuItem viewAllProductToolStripMenuItem;
+        private ToolStripMenuItem btnViewAllProduct;
         private ToolStripMenuItem ProductManagement;
         private ToolStripMenuItem orderManagement;
         private ToolStripMenuItem viewHistoryOrder;
@@ -232,6 +232,6 @@
         private ToolStripMenuItem btnViewProductAdmin;
         private ToolStripMenuItem btnAddNewProductAdmin;
         private ToolStripMenuItem btnViewAllOrderAdmin;
-        private Label lbHello;
+        private ToolStripMenuItem lbHello;
     }
 }
--- Assignment2/SalesWinApp/frmMain.cs
+++ Assignment2/SalesWinApp/frmMain.cs
@@ -8,20 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BusinessObject;
+using Microsoft.Extensions.Configuration;
 
 namespace SalesWinApp
 {
     public partial class frmMain : Form
     {
         private static MemberObject member = frmLogin.member;
+        private static string AdminEmail()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json", true, true)
+                .Build();
+            return config["AdminAccount:Email"];
+        }
+        private static string AdminPassword()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json", true, true)
+                .Build();
+            return config["AdminAccount:Password"];
+        }
         public frmMain()
         {
             InitializeComponent();
-        }
-
-        private void viewAllProductToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void btnViewProductAdmin_Click(object sender, EventArgs e)
@@ -46,7 +58,9 @@ namespace SalesWinApp
 
         private void btnViewAllMemberAdmin_Click(object sender, EventArgs e)
         {
-
+            frmMembers memberForm = new frmMembers();
+            memberForm.MdiParent = this;
+            memberForm.Show();
         }
 
         private void btnAddNewMemberAdmin_Click(object sender, EventArgs e)
@@ -56,12 +70,40 @@ namespace SalesWinApp
 
         private void btnLogout_Click(object sender, EventArgs e)
         {
-
+            frmLogin login = new frmLogin();
+            login.Show();
+            this.Hide();
         }
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            lbHello.Text = "Hello " + member.City + " !";
+            if (member != null)
+            {
+                lbHello.Text = "Hello " + member.Email + " !";
+                btnAddNewMemberAdmin.Enabled = false;
+                btnAddNewProductAdmin.Enabled = false;
+                btnViewAllOrderAdmin.Enabled = false;
+                btnViewProductAdmin.Enabled = false;
+                ProductManagement.Enabled = false;
+                orderManagement.Enabled = false;
+                memberManagementToolStripMenuItem.Enabled = false;
+            }
+            else
+            {
+                lbHello.Text = "Hello " + AdminEmail() + " !";
+                btnAddNewMemberAdmin.Enabled = true;
+                btnAddNewProductAdmin.Enabled = true;
+                btnViewAllOrderAdmin.Enabled = true;
+                btnViewProductAdmin.Enabled = true;
+                ProductManagement.Enabled = true;
+                orderManagement.Enabled = true;
+                memberManagementToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void btnViewAllProduct_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
