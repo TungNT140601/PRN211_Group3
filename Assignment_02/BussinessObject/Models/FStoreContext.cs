2b4dde887444eccb5af011ae5eb525935d59d907
TungNT140601 <tungnt14062001@gmail.com>
2022-07-08 11:58:36 +0700

    up khung Ass2

---
+++ Assignment_02/BussinessObject/Models/FStoreContext.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BussinessObject.Models
+{
+    public partial class FStoreContext : DbContext
+    {
+        public FStoreContext()
+        {
+        }
+
+        public FStoreContext(DbContextOptions<FStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Member> Members { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<TblOrder> TblOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=12345;Database=FStore");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Member>(entity =>
+            {
+                entity.ToTable("Member");
+
+                entity.Property(e => e.MemberId).ValueGeneratedNever();
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Companyname)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("OrderDetail");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany()
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OrderDeta__Order__2A4B4B5E");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany()
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OrderDeta__Produ__2B3F6F97");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ProductId).ValueGeneratedNever();
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.Property(e => e.Weight)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<TblOrder>(entity =>
+            {
+                entity.HasKey(e => e.OrderId)
+                    .HasName("PK__tbl_Orde__C3905BCF7958B292");
+
+                entity.ToTable("tbl_Order");
+
+                entity.Property(e => e.OrderId).ValueGeneratedNever();
+
+                entity.Property(e => e.Freight).HasColumnType("money");
+
+                entity.Property(e => e.OrderDate).HasColumnType("datetime");
+
+                entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+
+                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Member)
+                    .WithMany(p => p.TblOrders)
+                    .HasForeignKey(d => d.MemberId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__tbl_Order__Membe__267ABA7A");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Assignment_02/BussinessObject/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BussinessObject.Models
+{
+    public partial class Member
+    {
+        public Member()
+        {
+            TblOrders = new HashSet<TblOrder>();
+        }
+
+        public int MemberId { get; set; }
+        public string Email { get; set; }
+        public string Companyname { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<TblOrder> TblOrders { get; set; }
+    }
+}
