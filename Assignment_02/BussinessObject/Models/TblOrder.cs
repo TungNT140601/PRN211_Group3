4c85dc97bf144c9e18ffc414025d701d57563f24
vuncse151184 <vu12ace3@gmail.com>
2022-07-11 17:22:16 +0700

    up main

--- Assignment_02/BussinessObject/Models/TblOrder.cs
+++ Assignment_02/BussinessObject/Models/TblOrder.cs
@@ -7,10 +7,6 @@ namespace BussinessObject.Models
 {
     public partial class TblOrder
     {
-        public TblOrder()
-        {
-            OrderDetails = new HashSet<OrderDetail>();
-        }
         public int OrderId { get; set; }
         public int MemberId { get; set; }
         public DateTime OrderDate { get; set; }
@@ -19,6 +15,5 @@ namespace BussinessObject.Models
         public decimal? Freight { get; set; }
 
         public virtual Member Member { get; set; }
-        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
--- Assignment_02/DataAccess/Repositories/IMemberRopository.cs
+++ Assignment_02/DataAccess/Repositories/IMemberRopository.cs
@@ -5,5 +5,11 @@ namespace DataAccess.Repositories
     public interface IMemberRopository
     {
         public Member? CheckLogin(string email, string password);
+        public List<Member> GetMembers();
+        public Member GetMemberByID(int id);
+        public void AddMember(Member member);
+        public void UpdateMember(Member member);
+        public Member CheckEmail(string email);
+        public void DeleteMember(int id);
     }
 }
