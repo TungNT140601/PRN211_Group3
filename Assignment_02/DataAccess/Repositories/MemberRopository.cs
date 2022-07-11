f94e99ed49f2a738c4c916c5412b877f6650a3e5
DaoAnhTu21 <105470764+DaoAnhTu21@users.noreply.github.com>
2022-07-11 13:03:35 +0700

    upMemberDao, and frmMember

--- Assignment_02/DataAccess/Repositories/MemberRopository.cs
+++ Assignment_02/DataAccess/Repositories/MemberRopository.cs
@@ -7,5 +7,11 @@ namespace DataAccess.Repositories
     public class MemberRopository : IMemberRopository
     {
         public Member? CheckLogin(string email, string password) => MemberDAO.Instance.CheckLogin(email, password);
+        public List<Member> GetMembers() => MemberDAO.Instance.GetMembers();
+        public Member GetMemberByID(int id) => MemberDAO.Instance.GetMemberByID(id);
+        public void AddMember(Member member) => MemberDAO.Instance.AddMember(member);
+        public void UpdateMember(Member member) => MemberDAO.Instance.UpdateMember(member);
+        public Member CheckEmail(string email) => MemberDAO.Instance.CheckEmail(email);
+        public void DeleteMember(int id) => MemberDAO.Instance.DeleteMember(id);
     }
 }
