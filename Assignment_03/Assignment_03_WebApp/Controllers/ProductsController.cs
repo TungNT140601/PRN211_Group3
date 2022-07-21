bd5a9d9a8f5d4b64bf35c5c04f9c857a50591704
DaoAnhTu21 <105470764+DaoAnhTu21@users.noreply.github.com>
2022-07-21 19:23:33 +0700

    updateFormWeb

---
+++ Assignment_03/Assignment_03_WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assignment_03_Library.DataAccess;
+using Assignment_03_Library.Repositories;
+using System;
+namespace Assignment_03_WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        IProductRepository productRepository = null;
+        public ProductsController() => productRepository = new ProductRepository();
+        // GET: ProductController
+        public ActionResult ListProduct()
+        {
+            var product = productRepository.GetProducts();
+            return View(product);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = productRepository.GetProductById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create() => View();
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    productRepository.Add(product);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(product);
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = productRepository.GetProductById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)
+        {
+            try
+            {
+                if (id != product.ProductId)
+                {
+                    return NotFound();
+                }
+                if (ModelState.IsValid)
+                {
+                    productRepository.Update(product);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = productRepository.GetProductById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                productRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+    }
+}
